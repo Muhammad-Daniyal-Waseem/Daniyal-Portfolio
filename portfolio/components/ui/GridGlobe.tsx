717b24c75c96cc69533632909f91ccdb7dfa8f7e
Muhammad-Daniyal-Waseem <daniyalwaseem0808@gmail.com>
2024-07-03 05:22:43 +0500

    ui completed

--- portfolio/components/ui/GridGlobe.tsx
+++ portfolio/components/ui/GridGlobe.tsx
@@ -572,7 +572,7 @@ export function GlobeDemo() {
           </motion.div>
           <div className="absolute w-full bottom-0 inset-x-0 h-20 bg-gradient-to-b pointer-events-none select-none from-transparent to-black z-30  " />
           <div className="absolute w-full h-72 md:h-full z-10 bottom-24 sm:bottom-1 ">
-            <World data={sampleArcs} globeConfig={globeConfig} />;
+            <World data={sampleArcs} globeConfig={globeConfig} />
           </div>
         </div>
       </div>
